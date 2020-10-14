b81c1190e248aade0441fc4395df8f63dfcd1488
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2020-10-14 13:23:53 +0100

    Styling of the home page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Week1 from './components/week1';
 import Week2 from './components/week2';
+import styled from 'styled-components';
 import css from './App.css';
+import Week1Display from './assets/week1-display.jpg';
+
+const OuterWrapper = styled.div`
+  margin: 0 auto;
+  height:100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #e4ecf8;
+  text-align: center;
+`;
+
+const li = styled.li`
+  color: #000;
+`;
 
 const Home = () => (
   <div>
     <h1>Interaction Design</h1>
     <nav>
-      <ul>
-        <li>
-          <Link to="/week-1">Week 1 Splash Screen</Link>
+      <ul class="flex-container">
+        <li class="flex-item">
+          <Link to="/week-1">
+            Week 1 Splash Screen
+            <img src="./assets/week1-display.jpg" class="week-display"/>
+          </Link>
         </li>
-        <li>
+        <li class="flex-item">
           <Link to="/week-2">Week 2 Notification</Link>
         </li>
       </ul>
@@ -22,19 +41,21 @@ const Home = () => (
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/week-1">
-          <Week1 />
-        </Route>
-        <Route path="/week-2">
-          <Week2 />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <OuterWrapper>
+      <Router>
+        <Switch>
+          <Route path="/week-1">
+            <Week1 />
+          </Route>
+          <Route path="/week-2">
+            <Week2 />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </OuterWrapper>
   );
 }
 
