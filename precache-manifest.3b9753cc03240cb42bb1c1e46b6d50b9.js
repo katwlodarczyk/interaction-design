2ee5b8c3202dbd90ba921ac7a8c10cb1cefdea53
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2020-10-14 11:24:12 +0100

    Updates

---
+++ precache-manifest.3b9753cc03240cb42bb1c1e46b6d50b9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c693028721b64acf09327f7b711747",
+    "url": "/interaction-design/index.html"
+  },
+  {
+    "revision": "5e2709e866023707b027",
+    "url": "/interaction-design/static/css/main.0c6454a2.chunk.css"
+  },
+  {
+    "revision": "9d8400ad8bf5e2821f6d",
+    "url": "/interaction-design/static/js/2.5348c716.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/interaction-design/static/js/2.5348c716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2709e866023707b027",
+    "url": "/interaction-design/static/js/main.7b07b205.chunk.js"
+  },
+  {
+    "revision": "2810460e4a2244f3cbbe",
+    "url": "/interaction-design/static/js/runtime-main.ec7e679c.js"
+  },
+  {
+    "revision": "7c0104c7d9c1324d7313cc8bac467195",
+    "url": "/interaction-design/static/media/blob.7c0104c7.png"
+  },
+  {
+    "revision": "d8e648cf33aecf04bebfca17b914bc35",
+    "url": "/interaction-design/static/media/shopping-cart-gif.d8e648cf.gif"
+  }
+]);
