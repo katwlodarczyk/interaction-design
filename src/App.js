ff398d01fb577d048070caa6068db60582ddea1a
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2020-10-14 12:28:44 +0100

    week2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Week1 from './components/week1';
 import Week2 from './components/week2';
 import css from './App.css';
 
-
-
-const App = () => (
+const Home = () => (
   <div>
-    <Week1></Week1>
+    <h1>Interaction Design</h1>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/week-1">Week 1 Splash Screen</Link>
+        </li>
+        <li>
+          <Link to="/week-2">Week 2 Notification</Link>
+        </li>
+      </ul>
+    </nav>
   </div>
-);
+)
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/week-1">
+          <Week1 />
+        </Route>
+        <Route path="/week-2">
+          <Week2 />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 
 export default App;
--- src/components/week2/index.js
+++ src/components/week2/index.js
@@ -1,5 +1,4 @@
-import React from "react";
-import shoppingCart from "./../../assets/shopping-cart-gif.gif";
+import React, {useState} from "react";
 import styled from 'styled-components';
 import Notification from './notification';
 
@@ -39,16 +38,41 @@ const StyledParagraph = styled.p`
     z-index: 10000;
     font-family: 'Montserrat', sans-serif;
 `;
-  
+
+const StyledDiv = styled.div`
+border: 1px solid red;
+display: flex;
+justify-content: space-around;
+`;
+
+const Alert = () => {
+    const [visible, setVisible] = useState(true);
+
+    const handleClick = (e) => {
+        setVisible(false);
+    
+    }
+
+    return (
+        <>
+        {
+            visible && (
+            <StyledDiv>
+                <p>hello</p>
+                <span onClick={handleClick}>X</span>
+            </StyledDiv>
+            )
+        }
+        </>
+
+    );
+};
 
 function Week2() {
     return (
-        <OuterWrapper>
-            
-            <StyledWrapper>
-                    
-            </StyledWrapper>
-        </OuterWrapper>
+
+        <Alert />
+     
     );
 }
 
