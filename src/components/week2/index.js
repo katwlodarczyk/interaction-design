8ca7ea8ae03a50e0a930fa4bb3a64cc793db3647
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2020-10-16 11:28:08 +0100

    Work on the iphone structure

--- src/components/week2/index.js
+++ src/components/week2/index.js
@@ -1,5 +1,11 @@
 import React, {useState} from "react";
+import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import goback from './../../assets/back-icon.svg';
+import iphone from './../../assets/iphone-x.png';
+import flashlight from './../../assets/flashlight-icon.svg';
+import camera from './../../assets/camera-icon.svg';
+import wallpaper from './../../assets/wallpaper.jpg';
 import Notification from './notification';
 
 const OuterWrapper = styled.div`
@@ -40,40 +46,154 @@ const StyledParagraph = styled.p`
 `;
 
 const StyledDiv = styled.div`
-border: 1px solid red;
-display: flex;
-justify-content: space-around;
+   position: relative;
+   min-width: 308px;
+   min-height: 615px;
 `;
 
-const Alert = () => {
-    const [visible, setVisible] = useState(true);
+const Intersection = styled.div`
+   position: relative;
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-start;
+   padding-top: 100px;
+   width: 100%;
+   height: 100%;
+`;
 
-    const handleClick = (e) => {
-        setVisible(false);
-    
-    }
+const StyledIphone = styled.img`
+    filter: drop-shadow(20px 24px 32px rgba(0, 0, 0, 0.251783));
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 5;
+`;
+
+const Wallpaper = styled.img`
+    position: absolute;
+    top: 0;
+    right: 7px;
+    z-index: -5;
+    width: 292px;
+    height: 606px;
+    border-radius: 60px;
+    object-fit: cover;
+`; 
+const ClockDiv = styled.div`
+    position:relative;
+    font-size: 50px;
+    display: flex;
+    justify-content: center;
+    color: white;
+`;
+
+const DateDiv = styled.div`
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    position:relative;
+    color: white;
+`;
+
+const FunctionButtonsSection = styled.div`
+   display: flex;
+   align-content: end;
+   flex-direction: row;
+   justify-content: space-around;
+   margin: 0 20px;
+   padding-top: 324px;
+`;
+
+const CameraButton = styled.a`
+    padding: 10px 10px;
+    background-color: #dadada;
+    opacity: 0.6;
+    border-radius: 100px;
+`;
+
+const FlashlightButton = styled.a`
+    padding: 10px 10px;
+    background-color: #dadada;
+    opacity: 0.6;
+    border-radius: 100px;
+`;
+
+const Camera = styled.img`
+    width: 24px;
+    height: 24px;
+    opacity:1;
+`;
+
+const Flashlight = styled.img`
+    width: 24px;
+    height: 24px;
+    opacity:1;
+`;
+
+function Clock(props) {
 
     return (
-        <>
-        {
-            visible && (
-            <StyledDiv>
-                <p>hello</p>
-                <span onClick={handleClick}>X</span>
-            </StyledDiv>
-            )
-        }
-        </>
-
+      <div>
+         {props.date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}
+      </div>
     );
-};
+  }
+  
+  function tick() {
+    ReactDOM.render(
+      <Clock date={new Date()} />,
+      document.getElementById('time')
+    );
+  }
+  
+  setInterval(tick, 1000);
+  
+//   function Date(props) {
+
+//     return (
+//       <div>
+//          {props.date.toLocaleDateString()}
+//       </div>
+//     );
+//   }
+  
+//   function date() {
+//     ReactDOM.render(
+//       <Date date={new Date()} />,
+//       document.getElementById('date')
+//     );
+//   }
+  
+//   setInterval(tick, 1000);
+  
+
 
 function Week2() {
     return (
-
-        <Alert />
-     
-    );
+        <OuterWrapper>
+            <a href="./">
+                <img src={goback} className="go-back"></img>
+            </a>
+            <StyledWrapper>
+                <StyledDiv>   
+                    <StyledIphone src={iphone} className="iphone"></StyledIphone>
+                    <Wallpaper src={wallpaper}></Wallpaper>
+                    <Intersection className="intersection">
+                       <ClockDiv id="time"></ClockDiv>
+                       <DateDiv id="date">Friday 16 October</DateDiv>
+                       <FunctionButtonsSection>
+                            <FlashlightButton href="/#" className="flashlight-button">
+                                <Flashlight src={flashlight}></Flashlight>
+                            </FlashlightButton>
+                            <CameraButton href="/#" className="camera-button">
+                                <Camera src={camera}></Camera>
+                            </CameraButton>
+                       </FunctionButtonsSection>
+                    </Intersection>
+                </StyledDiv>
+            </StyledWrapper>
+        </OuterWrapper>
+);
 }
 
 
